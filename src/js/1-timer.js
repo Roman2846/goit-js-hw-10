2476e438bf7425928008f4dc7204221ef19a38ed
Roman <krasivskyi01@gmail.com>
2026-05-17 18:28:50 +0300

    hm-js-10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,97 @@
-console.log('wefwfwfwfwfw');
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let userSelectedDate = null;
+let intervalId = null;
+
+startBtn.disabled = true;
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateTimerUI({ days, hours, minutes, seconds }) {
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function startTimer() {
+  if (!userSelectedDate) return;
+
+  startBtn.disabled = true;
+  input.disabled = true;
+
+  intervalId = setInterval(() => {
+    const now = Date.now();
+    const diff = userSelectedDate - now;
+
+    if (diff <= 0) {
+      clearInterval(intervalId);
+      updateTimerUI({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+
+      input.disabled = false;
+      startBtn.disabled = true;
+      userSelectedDate = null;
+      return;
+    }
+
+    updateTimerUI(convertMs(diff));
+  }, 1000);
+}
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const now = Date.now();
+
+    if (!selectedDate || selectedDate.getTime() <= now) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
+
+      userSelectedDate = null;
+      startBtn.disabled = true;
+      return;
+    }
+
+    userSelectedDate = selectedDate.getTime();
+    startBtn.disabled = false;
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+startBtn.addEventListener('click', startTimer);
